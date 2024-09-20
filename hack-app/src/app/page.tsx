23c7a6d79ee30c14c47cbc41749aa15a510cdc7b
Luis Fernando Leal Contreras <l20121006@morelia.tecnm.mx>
2024-09-20 10:04:44 -0600

    DaisyUI y fontawesome instalados

--- hack-app/src/app/page.tsx
+++ hack-app/src/app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Home() {
+  const openModal = () => {
+    const modal = document.getElementById('my_modal_1') as HTMLDialogElement | null;
+    if (modal) {
+      modal.showModal();
+    }
+  };
+
   return (
-        <div className="flex justify-between items-center w-full min-h-screen">
+    <div className="flex justify-between items-center w-full min-h-screen">
       <div className="w-1/3 p-10">
         <div className="max-w-md relative flex flex-col p-4 rounded-md text-black bg-white">
           <div className="text-2xl font-bold mb-2 text-[#1e0e4b] text-center">
@@ -36,6 +46,19 @@ export default function Home() {
       </div>
       <div className="w-2/3">
         <p>Hola</p>
+        <button className="btn" onClick={openModal}>open modal</button>
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">Press ESC key or click the button below to close</p>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
       </div>
     </div>
   );
